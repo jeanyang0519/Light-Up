6d02872a4d02ac3f30b01261c369a2ba0fe09077
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2019-12-30 11:37:24 -0500

    Setup Backend User Auth

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+const passport = require("passport");
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(()=> console.log("Connected to MongoDB successfully"))
     .catch( err => console.log(err))
 
-app.get("/", (req, res) => res.send("Hello World its me from express"));
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users)
+
