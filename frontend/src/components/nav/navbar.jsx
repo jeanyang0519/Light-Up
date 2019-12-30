e2f00ddf76eb266ad26a66647784dd4b39535559
App Academy Student <email@email.com>
2019-12-30 11:40:00 -0500

    frontend user auth almost finished

---
+++ frontend/src/components/nav/navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  getLinks() {
+    if (this.props.loggedIn) {
+      return (
+        <div>
+          <Link to={'/messaging'}>Messaging</Link>
+          <Link to={'/profile'}>Profile</Link>
+          <button onClick={this.logoutUser}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to={'/signup'}>Sign up</Link>
+          <Link to={'/login'}>Login</Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Light Up</h1>
+        {this.getLinks()}
+      </div>
+    );
+  }
+}
+
+export default NavBar;
