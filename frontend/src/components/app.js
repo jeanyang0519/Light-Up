f80dfc40a82dca1d2ea9498a8a482cd76e148709
Jean <sy2803@tc.columbia.edu>
2020-01-02 11:06:41 -0500

    before pushing to heroku

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -9,6 +9,15 @@ import IndexContainer from './index/index_container';
 import ProfileContainer from './index_show_item/profile_container';
 import Chat from './chat/chat';
 
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const App = () => (
   <div>
 
