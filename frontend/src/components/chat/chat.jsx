4d1ea3454ae79b831bea57b3a373f1dc0968ef2f
Alaizard <jtkopacz@gmail.com>
2020-01-02 17:15:38 -0500

    merging show page stylez

--- frontend/src/components/chat/chat.jsx
+++ frontend/src/components/chat/chat.jsx
@@ -7,7 +7,7 @@ class Chat extends React.Component {
     super(props);
     socket.emit("enter chat", "5e0d8f2119c96fcdcd15c2df");
     socket.on("refresh messages", data => {
-        // console.log("about to refresh messages", data);
+        console.log("about to refresh messages", data);
         // fetch chat needs to go on chat_container
         fetchChats("5e0a579d6a0bd7e29328b9b7");
     });
