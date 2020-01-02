5c8115bf619da27455a77115e81e9c0570ac0405
Jean <sy2803@tc.columbia.edu>
2020-01-02 11:22:39 -0500

    app

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const server = app.listen(port, () => console.log(`Server is running on port ${p
 const passport = require("passport");
 const io = require("socket.io").listen(server)
 
+
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(()=> console.log("Connected to MongoDB successfully"))
