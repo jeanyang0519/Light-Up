1588ea9c0d492e4569f1a30e91c9af4570e073eb
Yorkis Diaz <yorkis.i.diaz@gmail.com>
2020-01-02 19:06:59 -0500

    Fix bugs of server

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,12 +6,11 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import IndexContainer from './index/index_container';
 import ProfileContainer from './index_show_item/profile_container';
-<<<<<<< HEAD
 import Chat from './chat/chat';
 import NavBar from '../components/nav/navbar_container';
-=======
+
 import ChatIndexContainer from './chat/chats_index_container';
->>>>>>> 2c84a5a4a71d8f75282f19656abb65ab88614874
+
 
 
 const App = () => (
@@ -24,12 +23,8 @@ const App = () => (
     </Switch>
       <Route exact path="/dashboard" component={IndexContainer} />
       <Route exact path="/profile/:id" component={ProfileContainer} />
-<<<<<<< HEAD
-      <Route path="/chat" component={Chat}/>
-=======
       <Route path="/chat" component={ChatIndexContainer}/>
-    </Switch>
->>>>>>> 2c84a5a4a71d8f75282f19656abb65ab88614874
+
   </div>
 );
 
