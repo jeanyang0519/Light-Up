2ed9945df6a1d7285d411edcad9669d0aad63788
App Academy Student <aa@Apps-Mac-mini-14.local>
2020-01-21 18:22:15 -0500

    stuck on separating connections and users

--- frontend/src/components/index/index.jsx
+++ frontend/src/components/index/index.jsx
@@ -12,24 +12,50 @@ class Index extends React.Component {
   }
 
   render() {
-    
-      const users = this.props.users.map((user, i) => {
-        return (
-          <Link className='user-profile' to={`/profile/${user.id}`} key={i}> 
-            <li className='each-user' key={user.id}>{user.username}</li>
-          </Link>
-          //comment
-        )
-      })
 
-    return(
+    let connections = [];
+    // this.props.connections.map((connection, i) => {
+    //   return (
+    //     <Link className='user-profile' to={`/profile/${connection.user._id}`} key={i}>
+    //       <li className='each-user' key={connection.user._id}>{connection.user.username}</li>
+    //     </Link>
+    //   )
+    // })
+    let arr = [];
+      this.props.users.forEach((user, i) => {
+        for (let j = 0; j < this.props.connections.length; j++) {
+            let connection = this.props.connections[j];
+            if (user.id === connection.user._id) {
+              return connections.push(
+                <Link className='user-profile' to={`/profile/${user.id}`} key={i}>
+                  <li className='each-user' key={user.id}>{user.username}</li>
+                </Link>
+              )
+            } else {
+               return arr.push(
+                <Link className='user-profile' to={`/profile/${user.id}`} key={i}> 
+                  <li className='each-user' key={user.id}>{user.username}</li>
+                </Link>
+              )
+            }  
+        }
+          
+      })
+      debugger
+      return(
         
       <div className='index-all'>
         {/* <NavContainer/> */}
+        <div className='connections-wrapper'>
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"></link>
+          <div className='connection'>
+            {connections}
+          </div>
+        </div>
         <div className='user-wrapper'>
           <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"></link>
           <div className='user'>
-            {users}
+            {arr}
           </div>
         </div>
       </div>
